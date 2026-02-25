014944cc8a578a4f00eacdfa75bf0c8924e4f360
Flammie A Pirinen <flammie@iki.fi>
2026-02-25 16:31:29 +0100

    more verbosity and lemma testing logics

--- giellaltlextools/gtlemmatest.py
+++ giellaltlextools/gtlemmatest.py
@@ -33,7 +33,7 @@ def main():
                       required=True)
     argp.add_argument("-T", "--threshold", type=int,
                       help="required percentage of succesful generations",
-                      default=99)
+                      default=100)
     argp.add_argument("-d", "--debug", action="store_true", default=False,
                       help="prints debugging outputs")
     argp.add_argument("-v", "--verbose", action="store_true", default=False,
@@ -42,7 +42,7 @@ def main():
                       help="do not count oov if analysis contained in file")
     argp.add_argument("-X", "--exclude", action="append",
                       help="exclude lines matching regex")
-    argp.add_argument("-Q", "--oov-limit", type=int, default=1000,
+    argp.add_argument("-Q", "--oov-limit", type=int, default=10_000,
                       help="stop trying after so many oovs")
     argp.add_argument("-B", "--time-out", type=int, default=60,
                       help="max time to use with lemmas")
@@ -74,36 +74,55 @@ def main():
         print(f"used {end-start} times for lemma scraping")
     lines = 0
     oovs = 0
+    misses = 0
     start = time()
     timedout = False
     for lemma in lemmas:
         if lemma in {"", "#", "#;"}:
             continue
-        failed = True
+        empty = True
+        matched = False
+        mismatches = set()
         for tagstring in options.tags:
             generations = generator.lookup(lemma + tagstring)
             if len(generations) == 0:
                 if options.verbose:
                     print(f"{lemma}{tagstring} does not generate!")
             else:
-                failed = False
+                empty = False
+                for generation in generations:
+                    if generation[0] == lemma:
+                        matched = True
+                    else:
+                        mismatches.add(f"{lemma}{tagstring} => {generation}")
         if skipforms and lemma in skipforms:
             continue
         lines += 1
-        if failed:
+        if empty:
             oovs += 1
             for tagstring in options.tags:
-                print(f"{lemma}{tagstring}", file=logfile)
+                print(f"{lemma}{tagstring} => ?", file=logfile)
+        if not matched:
+            misses += 1
+            for mismatch in mismatches:
+                print(mismatch, file=logfile)
+        if empty or not matched:
             analyses = analyser.lookup(lemma)
             if len(analyses) > 0:
                 print(f"\tN.B: {lemma} has following analyses", file=logfile)
+                uniques = set()
                 for analysis in analyses:
+                    uniques.add(analysis[0])
+                for analysis in uniques:
                     print(f"\t{analysis}", file=logfile)
                     if options.verbose:
                         print(f"\t{analysis}")
+            else:
+                print(f"\t{lemma} has no analyses either", file=logfile)
             if oovs >= options.oov_limit:
                 print("too many fails, bailing to save time...")
                 break
+
         now = time()
         if now - start > options.time_out:
             print(f"bailing after timeout: {now - start}")
@@ -115,36 +134,39 @@ def main():
     if lines == 0:
         print(f"SKIP: could not find lemmas in {options.lexcfile.name}")
         sys.exit(77)
-    coverage = (1.0 - (float(oovs) / float(lines))) * 100.0
+    failures = oovs + misses
+    coverage = (1.0 - (float(failures) / float(lines))) * 100.0
     if options.verbose:
         print(f"used {end-start} times for generating")
         print("Lemma statistics:")
         print(f"\t{len(lemmas)} lemmas")
         print(f"\t{coverage} % success")
     if coverage < options.threshold:
-        print("FAIL: too many lemmas weren't generating!",
+        print("FAIL: too many lemmas wrong!",
               f"{coverage} < {options.threshold}")
-        print(f"see {logfile.name} for details ({oovs} ungenerated strings)")
+        print(f"see {logfile.name} for details")
+        print(f"({oovs} ungenerated strings, {misses} wrong lemmas)")
         if options.editor:
             Popen([options.editor, logfile.name])
         sys.exit(1)
     else:
-        if timedout and oovs > 0:
+        if timedout and failures > 0:
             print("FAIL: timed out with ungenerated lemmas")
-            print(f"see {logfile.name} for details ({oovs} ungenerated strings)")
+            print(f"see {logfile.name} for details")
+            print(f"({oovs} ungenerated strings, {mismatches} wrong lemmas)")
             if options.editor:
                 Popen([options.editor, logfile.name])
             sys.exit(1)
         elif timedout:
             print("SKIP: timed out but didn't find  problems")
             sys.exit(77)
-        elif oovs > 0:
+        elif failures > 0:
             print("SUCCESS: within threshold!",
                   f"{coverage} >= {options.threshold})")
             if options.editor:
                 Popen([options.editor, logfile.name])
                 sys.exit(0)
-    if oovs > 0:
+    if failures > 0:
         print("SUCCESS: within threshold!",
               f"{coverage} >= {options.threshold})")
         if options.editor:
