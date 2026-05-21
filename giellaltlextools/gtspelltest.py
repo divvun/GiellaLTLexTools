cda1224f8223b80aa744e18bd47e5e7b4c02ed1b
Flammie A Pirinen <flammie@iki.fi>
2026-05-21 15:21:47 +0200

    use config.json for spelltestings

--- giellaltlextools/gtspelltest.py
+++ giellaltlextools/gtspelltest.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python3
 """Lemma testing for GiellaLT spell-checkers and lexicons."""
 
+import json
+import re
 import subprocess
 import sys
 import tempfile
-import re
-from argparse import ArgumentParser
+from argparse import ArgumentParser, FileType, Namespace
+from os.path import basename
 from subprocess import Popen
+from typing import TextIO
 
 from termcolor import colored, cprint
 
 from . import __version__
 from .lexc import scrapelemmas
-
 
 DEFAULT_EXCLUSIONS = [
     r"\+Use/-Spell",
@@ -25,6 +27,15 @@ DEFAULT_EXCLUSIONS = [
     r"\+CmpNP/Suff",
     r"\+CmpNP/Only",
 ]
+
+def prettyprint_json(config):
+    """Pretty-print a JSON config."""
+    prettyfiles = []
+    for lexcfilename in config["lexcfiles"]:
+        prettyfiles.append(".../" + basename(lexcfilename))
+    config["lexcfiles"] = prettyfiles
+    config["zhfstfile"] = ".../" + basename(config["zhfstfile"])
+    return json.dumps(config, indent=4, sort_keys=True)
 
 
 def parse_input_lemma(line: str) -> str:
@@ -40,14 +51,9 @@ def parse_input_lemma(line: str) -> str:
 def main():
     """CLI for speller lemma testing."""
     argp = ArgumentParser()
-    argp.add_argument("lexcfilenames", nargs="+",
-                      help="read lemmas from the lexc files")
     argp.add_argument("-V", "--version", action="version",
                       version=f"%(prog)s {__version__}",
                       help="print version info")
-    argp.add_argument("-z", "--zhfst", type=str, dest="zhfstfilename",
-                      help="ZHFST speller for analysing missing lemmas",
-                      required=True)
     argp.add_argument("-D", "--runner", type=str, dest="runnerfilename",
                       help="external runner capable of handling zhfst",
                       required=True)
@@ -58,40 +64,50 @@ def main():
                       help="prints debugging outputs")
     argp.add_argument("-v", "--verbose", action="store_true", default=False,
                       help="prints some outputs")
-    argp.add_argument("-Z", "--acceptable-forms", type=open,
-                      help="do not count oov if analysis contained in file")
-    argp.add_argument("-X", "--exclude", action="append",
-                      help="exclude lines matching regex")
     argp.add_argument("-Q", "--oov-limit", type=int, default=100_000,
                       help="stop trying after so many oovs")
     argp.add_argument("-B", "--time-out", type=int, default=60,
                       help="max time used to test lemmas")
     argp.add_argument("-E", "--editor", type=str,
                       help="open failures in EDITOR afterwards")
+    argp.add_argument("-c", "--config", type=open, metavar="CONFIGFILE",
+                      help="read configuration from CONFIGFILE", required=True)
+    argp.add_argument("-L", "--logfile", type=FileType("w"), metavar="LOGFILE",
+                      help="save permanent markdown log in LOGFILE")
     options = argp.parse_args()
-    logfile = tempfile.NamedTemporaryFile(prefix="gtlemmaspell", suffix=".txt",
-                                          delete=False, encoding="UTF-8",
-                                          mode="w+")
+    if options.logfile:
+        dostuff(options, options.logfile)
+    else:
+        with tempfile.NamedTemporaryFile(prefix="gtlemmaspell", suffix=".md",
+                                         delete=False, encoding="UTF-8",
+                                         mode="w+") as logfile:
+            dostuff(options, logfile)
+
+
+def dostuff(options: Namespace, logfile: TextIO):
+    """Run spell-checking tests for lemmas."""
+    configuration = json.load(options.config)
     if "divvunspell" in options.runnerfilename:
         spellargs = [options.runnerfilename, "suggest", "--archive",
-                     options.zhfstfilename]
+                     configuration["zhfstfile"]]
     elif "hfst-ospell" in options.runnerfilename:
-        spellargs = [options.runnerfilename, "-S", options.zhfstfilename]
+        spellargs = [options.runnerfilename, "-S", configuration["zhfstfile"]]
     else:
         print(colored("fail", "red"),
               f"- unknown runner {options.runnerfilename}")
         sys.exit(1)
     skipforms = None
-    if options.acceptable_forms:
-        skipforms = [l.strip() for l in options.acceptable_forms.readlines()]
+    if "acceptable_form_file" in configuration:
+        with open(configuration["acceptable_form_file"],
+                  encoding="utf-8") as acceptable_forms:
+            skipforms = [l.strip() for l in acceptable_forms.readlines()]
+    exclusions = DEFAULT_EXCLUSIONS.copy()
+    if "exclusions" in configuration:
+        exclusions.extend(configuration["exclusions"])
     lemmas = set()
-    if options.exclude:
-        options.exclude.extend(DEFAULT_EXCLUSIONS)
-    else:
-        options.exclude = DEFAULT_EXCLUSIONS.copy()
-    for lexcfilename in options.lexcfilenames:
+    for lexcfilename in configuration["lexcfiles"]:
         with open(lexcfilename, encoding="utf-8") as lexcfile:
-            more = scrapelemmas(lexcfile, options.exclude, options.debug)
+            more = scrapelemmas(lexcfile, exclusions, options.debug)
             for lemma in more:
                 lemmas.add(lemma)
     lines = 0
@@ -110,6 +126,7 @@ def main():
     skipping = True
     if options.verbose:
         print("processing done.")
+    print("# Results for lemmatesting spell-checker\n", file=logfile)
     for line in results.stdout.decode("utf-8").strip().split("\n"):
         if "Input:" in line:
             lemma = parse_input_lemma(line)
@@ -126,28 +143,33 @@ def main():
             oovs += 1
             if options.verbose:
                 print(f"{lemma} is not accepted")
-            print(f"{lemma}", file=logfile)
-            print("\tfollowing suggestions:", file=logfile)
+            print(f"\n**{lemma}** is missing. ", file=logfile)
+            print("following suggestions:", file=logfile)
         else:
             if "Input:" not in line:
-                print(f"\t{line}", file=logfile)
+                print(f"* {line}", file=logfile)
         if oovs >= options.oov_limit:
             print("too many fails, bailing to save time...")
             break
     if lines == 0:
         print(colored("SKIP:", "cyan"),
-              f"could not find lemmas in {options.lexcfilenames}")
+              f"could not find lemmas in {configuration["lexcfiles"]}")
         sys.exit(77)
     coverage = (1.0 - (float(oovs) / float(lines))) * 100.0
     if options.verbose:
         print("Lemma statistics:")
         print(f"\t{len(lemmas)} lemmas")
         print(f"\t{coverage} % accepted")
+    print("\n## Lemma statistics:\n", file=logfile)
+    print(f"* {len(lemmas)} lemmas", file=logfile)
+    print(f"* {coverage} % accepted", file=logfile)
+    prettyconfig = prettyprint_json(configuration)
+    print(f"\n## Configuration:\n\n```json\n{prettyconfig}\n```", file=logfile)
     if coverage < options.threshold:
         print(colored("FAIL:", "red"), f"{oovs} lemmas failed!",
               f"({coverage} % < {options.threshold} %)")
         print("fix lemmas in follwoing files please:",
-              colored(options.lexcfilenames, "cyan"))
+              colored(configuration["lexcfiles"], "cyan"))
         print("see", colored(logfile.name, "magenta"), "for details")
         if options.editor:
             print(f"Running {options.editor} {logfile.name}...")
@@ -158,7 +180,7 @@ def main():
               f"{len(lemmas)} lemmas {coverage} % accepted")
         if coverage < 100:
             print("fix lemmas in follwoing files please:",
-                  colored(options.lexcfilenames, "cyan"))
+                  colored(configuration["lexcfiles"], "cyan"))
             print("see", colored(logfile.name, "magenta"), "for details")
 
 
